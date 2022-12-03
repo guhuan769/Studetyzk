e9d03d2eb9f65437e2bb759ad2b946660489df92
guhuan769 <769540542@qq.com>
2022-12-03 14:43:12 +0800

    demo

---
+++ awaitasync1/awaitasync1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Data.SqlTypes;
+
+// See https://aka.ms/new-console-template for more information
+string filename = @"D:\test\1.txt";
+await File.WriteAllTextAsync(filename, "hello");//异步方法写入
+Console.WriteLine("Hello, World!");
+//读出文件
+string text = await File.ReadAllTextAsync(filename);
+Console.WriteLine($"{text}");
+main();
+void main()
+{
+    Console.WriteLine("132");
+
+}
