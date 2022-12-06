63682c5402aa36e0f44a47c388813f1343563925
guhuan769 <769540542@qq.com>
2022-12-06 19:15:08 +0800

    Add Read Json Config

---
+++ awaitasync1/ConsoleApp1ReadJson/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace ConsoleApp1ReadJson
+{
+    /*
+        Microsoft.Extensions.Configuration
+        Microsoft.Extensions.Configuration.Json 
+        microsoft.extensions.configuration.binder.7.0.0.nupkg
+        注意:一定要将Config.json文件 复制到输出目录 选择 如果较新则复制 不然就会出现无法读取
+     */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
+            configurationBuilder.AddJsonFile("Config.json", optional: true, reloadOnChange: true);
+            IConfigurationRoot configurationRoot = configurationBuilder.Build();
+            string name = configurationRoot["name"];
+            Console.WriteLine($"name={name}");
+            //string add = configurationRoot.GetSection("proxy:address").Value;
+            //Console.WriteLine($"add={add}");
+            var get = configurationRoot.GetSection("proxy").Get<Proxy>();
+        }
+    }
+
+    class Proxy
+    {
+        public string address { get; set; }
+    }
+}
