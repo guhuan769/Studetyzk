17ce7635b825e35af42f9e071d7031509fa78ea6
guhuan769 <769540542@qq.com>
2022-12-07 18:58:21 +0800

    EF 增删改查

--- EfStudent/ConsoleApp1/ConsoleApp1/Program.cs
+++ EfStudent/ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace ConsoleApp1
+﻿using EFCore;
+
+namespace ConsoleApp1
 {
     /*
      Add-Migration Init  初始化
@@ -7,9 +9,48 @@
      */
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //using避免资源泄露 myDbContext逻辑上的数据库
+            using (MyDbContext myDbContext = new MyDbContext())
+            {
+                #region 新增
+                //Dog dog = new Dog();
+                //dog.DogName = "test";
+                ////把Dog加入到逻辑上的表里头
+                //myDbContext.dog.Add(dog);
+
+                //Dog dog1 = new Dog();
+                //dog1.DogName = "test1";
+                ////把Dog加入到逻辑上的表里头
+                //myDbContext.dog.Add(dog);
+
+                //Book book = new Book()
+                //{ AuthorName="gh", Price=3, PubTime = DateTime.Now, Title="wa" };
+                //myDbContext.Books.Add(book); 
+                #endregion
+
+                #region 查询
+                //IQueryable<Book> books = myDbContext.Books.Where(x => x.Id == 1);
+
+                //foreach (var item in books)
+                //{
+                //    Console.WriteLine(item.Title);
+                //}
+
+                //var bookOne = myDbContext.Books.Single(x => x.Title.Equals("wa"));
+                //bookOne.Title = "美丽的第一本书";
+                //Console.WriteLine(bookOne.PubTime); 
+                #endregion
+
+                #region 删除
+                //IQueryable<Book> books = myDbContext.Books.Where(x => x.Id == 1);
+                var b = myDbContext.Books.Single(x => x.Id == 1);
+                myDbContext.Remove(b);
+                #endregion
+                int row = await myDbContext.SaveChangesAsync();//Update-Database 
+                Console.WriteLine($"影响行{row}");
+            }
         }
     }
 }
