77099618800b333f2e84fe50ee603be3250312c7
guhuan769 <769540542@qq.com>
2022-12-25 16:52:38 +0800

    123

---
+++ Rabbitmq/SenderTest1/ConsumerTest1/Program.cs
@@ -0,0 +1,38 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var connFactory = new ConnectionFactory();
+connFactory.HostName = "localhost";//127.0.0.1
+connFactory.DispatchConsumersAsync = true;
+var connection = connFactory.CreateConnection();//创建链接
+string exchangeName = "Exchange1";
+string queueName = "queue1";
+string routingKey = "key1";
+using var channel = connection.CreateModel();
+channel.ExchangeDeclare(exchangeName, "direct");
+channel.QueueDeclare(queueName, durable: true, exclusive: false, arguments: null);
+channel.QueueBind(queue: queueName, exchangeName, routingKey);
+//创建一个消费者 consumer
+AsyncEventingBasicConsumer consumer = new AsyncEventingBasicConsumer(channel);
+consumer.Received += Consumer_Received;
+channel.BasicConsume(queueName, autoAck: false, consumer: consumer);
+Console.WriteLine("按回车键退出");
+Console.ReadLine();
+async Task Consumer_Received(object sender, BasicDeliverEventArgs _event)
+{
+	try
+	{
+		byte[] bytes = _event.Body.ToArray();
+		string text = Encoding.UTF8.GetString(bytes);
+		Console.WriteLine(DateTime.Now + "收到消息:" + text);
+		//DeliveryTag 就是消息的编号
+		channel.BasicAck(_event.DeliveryTag, multiple: false);
+		await Task.Delay(800);
+	}
+	catch (Exception ex)
+	{
+		Console.WriteLine(ex);
+		channel.BasicReject(_event.DeliveryTag, true);
+	}
+}
---
+++ Rabbitmq/SenderTest1/SenderTest1/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RabbitMQ.Client;
+using System.Text;
+
+var connFactory = new ConnectionFactory();
+connFactory.HostName = "localhost";//127.0.0.1
+connFactory.DispatchConsumersAsync = true;
+var connection = connFactory.CreateConnection();//创建链接
+
+while (true)
+{
+    using var channel = connection.CreateModel();
+    var prop = channel.CreateBasicProperties();
+    prop.DeliveryMode = 2;
+    string exchangeName = "Exchange1";
+    //声明交换机
+    channel.ExchangeDeclare(exchangeName, "direct");
+    byte[] buffer = Encoding.UTF8.GetBytes(DateTime.Now.ToString());
+    channel.BasicPublish(exchangeName, routingKey: "key1", mandatory: true, basicProperties: prop, body: buffer);
+    Console.WriteLine("OK"+DateTime.Now);
+    Thread.Sleep(1000);
+}
